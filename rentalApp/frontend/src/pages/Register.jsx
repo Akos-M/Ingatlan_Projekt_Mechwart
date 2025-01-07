fab86634f476bacf464bb56eaf96c67d0f36ab08
VividBee <146666734+VividBee@users.noreply.github.com>
2025-01-07 13:17:23 +0100

    Update Register.jsx

--- rentalApp/frontend/src/pages/Register.jsx
+++ rentalApp/frontend/src/pages/Register.jsx
@@ -28,10 +28,28 @@ const Register = () => {
     };
 
     return (
+        <>
+            <style>
+                {`
+                    html, body {
+                        height: 100%;
+                        margin: 0;
+                    }
+                    body {
+                        background-image: url('src/pages/logbg.jpg');
+                        background-position: center;
+                        background-size: cover;
+                        background-attachment: fixed;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                    }
+                `}
+            </style>
         <div className="container mt-5">
             <div className="row justify-content-center">
                 <div className="col-md-6">
-                    <div className="card">
+                    <div className="card" style={{height:'330px',width:'400px',justifyContent:'center',marginRight:'250px',backgroundColor:'hsla(42, 24.80%, 69.20%, 0.60)'}}>
                         <div className="card-body">
                             <h3 className="card-title text-center">Register</h3>
                             {error && <p style={{ color: 'red' }}>{error}</p>}
@@ -62,7 +80,7 @@ const Register = () => {
                                         required
                                     />
                                 </div>
-                                <button type="submit" className="btn btn-primary w-100">Register</button>
+                                <button style={{backgroundColor:'hsl(33, 90.50%, 67.10%)', marginLeft:'90px'}} type="submit" className="btn btn-primary w-50">Register</button>
                             </form>
                             <button onClick={() => navigate('/')} className="btn btn-link mt-3 w-100">Back to Login</button>
                         </div>
@@ -70,6 +88,7 @@ const Register = () => {
                 </div>
             </div>
         </div>
+        </>
     );
 };
 
