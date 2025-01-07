bf1b422c99a067b76e7ed1cfab3d6428eb63ca1c
VividBee <146666734+VividBee@users.noreply.github.com>
2025-01-07 13:15:59 +0100

    Update Apartments.jsx
    Frissítés.

--- rentalApp/frontend/src/pages/Apartments.jsx
+++ rentalApp/frontend/src/pages/Apartments.jsx
@@ -28,22 +28,32 @@ const Apartments = () => {
         navigate('/add-apartment');
     };
 
+    const goToRegister = () => {
+        navigate('/register');
+    };
+
+    const goToLogin = () => {
+        navigate('/');
+    };
+
     return (
-        <div>
-            <nav className="navbar navbar-light bg-light">
-                <span className="navbar-brand mb-0 h1">Apartment Rental Project</span>
+        <div style={{ backgroundImage: "url('src/pages/mainbg.jpg')",backgroundPosition:'center',backgroundSize:'cover',backgroundAttachment:'fixed' }} >
+            <nav className="navbar navbar-light" style={{backgroundColor:'hsl(13, 65%, 56%)'}}>
+                <span style={{marginLeft:'20px'}} className="navbar-brand mb-0 h1">Apartment Rental Project</span>
                 <div>
-                    <button className="btn btn-outline-primary me-2" onClick={goToAddApartment}>Add Apartment</button>
-                    <button className="btn btn-outline-danger" onClick={handleLogout}>Logout</button>
+                    <button style={{backgroundColor:'hsl(39, 79.80%, 55.30%)'}} className="btn btn-outline-primary me-2" onClick={goToAddApartment}>Add Apartment</button>
+                    <button style={{backgroundColor:'hsl(39, 79.80%, 55.30%)'}} className="btn btn-outline-primary me-3" onClick={goToLogin}>Login</button>
+                    <button style={{backgroundColor:'hsl(39, 79.80%, 55.30%)'}} className="btn btn-outline-primary me-4" onClick={goToRegister}>Register</button>
+                    <button style={{backgroundColor:'hsl(39, 79.80%, 55.30%)',marginRight:'30px'}} className="btn btn-outline-danger" onClick={handleLogout}>Logout</button>
                 </div>
             </nav>
-            <div className="container mt-4">
-                <h1 >Available Apartments</h1>
-                <div className="row">
+            <div className="container mt-4" >
+                <h1 style={{fontFamily:'"Brush Script MT", cursive',fontSize:'70px'}}>Available Apartments</h1>
+                <div className="row g-4" style={{margin:'80px'}}>
                     {apartments.map((apartment) => (
-                        <div key={apartment.id} className="col-md-4">
-                            <div className="card mb-4">
-                                <img
+                        <div key={apartment.id} className="col-12 col-sm-6 col-md-4">
+                            <div className="card h-100" style={{margin:'20px',marginLeft:'10px',marginRight:'10px',backgroundColor:'hsla(37, 55.60%, 70.00%, 0.64)',border:'2px solid black'}}>
+                                <img style={{height:'300px',objectFit:'cover',border:'1px solid black'}}
                                     src={apartment.image}
                                     className="card-img-top"
                                     alt={apartment.name}
@@ -57,6 +67,7 @@ const Apartments = () => {
                     ))}
                 </div>
             </div>
+            <nav style={{backgroundColor:'hsl(13, 65%, 56%)',height:'30px'}}><span className="navbar-brand mb-0 h1">Made by M.Ákos - S.László - V.Gréta</span></nav>
         </div>
     );
 };
